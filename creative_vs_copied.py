0ae3410cacfc6db18a4f30ef23c957a8afb807d9
Deepesh Goel <deepeshgoel20@gmail.com>
2025-01-17 02:54:58 +0530

    Create creative_vs_copied.py

---
+++ creative_vs_copied.py
@@ -0,0 +1,152 @@
+from IPython.display import display, Markdown #
+
+# Colors to use in the Markdown Text
+md_colors = ["#009292", "#FF6DB6", "#490092", "#006DDB", "#B66DFF", "#920000", "#DB6D00"]
+
+def list_of_seqs_to_markdown(list_of_sequences):
+    """
+    Takes in a list-of-list-of-strings, and colors each sublist with a single
+    color cycling through the list `md_colors` to pick those colors
+
+    :param      list_of_sequences:  List of list of words
+    :type       list_of_sequences:  List of list of words
+    """
+
+    # The output string
+    display_string = ""
+    for i, word_sequence in enumerate(list_of_sequences):
+        display_string = display_string + f'<font color={md_colors[i % len(md_colors)]}>{" ".join(word_sequence)}</font> '
+
+    # Draw that string with Markdown
+    display(Markdown(display_string))
+
+
+class LcsFinder:
+    """
+    Internally, this class takes `word_sequence` and
+    builds a hash table from any k-gram that appears in
+    `word_sequence` to the list of all indices where that
+    k-gram appears in `word_sequence`. This makes finding
+    the LCS pretty fast.
+    It also stores a set of all less-than-k grams that appear
+    in the text, but just as a set and not as a hash table.
+    """
+
+    def __init__(self, word_sequence, k=3):
+        """
+        By default, this hash table uses 3-grams, this seems to work well. Feel
+        free to pass in a different k-value, though keep in mind this is a
+        heuristic speedup, and is unrelated to the k-gram from the language
+        model.
+
+        :param      word_sequence:  A book
+        :type       word_sequence:  List of strings
+        :param      k:              Heuristic k-gram size for efficient lookup
+        :type       k:              int
+        """
+        self.k = k
+        self.word_sequence = word_sequence
+        self.index_hash = dict()
+        self.short_gram_set = set()
+
+        for i in range(len(word_sequence)-k):
+
+            k_gram = " ".join(word_sequence[i:i+k])  # Build this k-gram
+            if k_gram not in self.index_hash:
+                self.index_hash[k_gram] = []  # Add it to the hash table if it ain't there
+            self.index_hash[k_gram].append(i)  # And add the index to the list
+
+            # Update the set of short grams with all possible grams starting at i, and with length < k.
+            self.short_gram_set.update({" ".join(word_sequence[i:i+j]) for j in range(1,k)})
+
+    def length_of_lcs_from_start(self, small_sequence, start_idx):
+        """
+        Look for the longest common substring of word_sequence and
+        small_sequence, subject to the substring starting at
+        `small_sequence[start_idx]`. Returns the length of this LCS, measured in
+        words.
+
+        :param      small_sequence:  A small word sequence
+        :type       small_sequence:  List of strings
+        :param      start_idx:       The index where the LCS has to start at
+        :type       start_idx:       int
+
+        :returns:   Length of the LCS, measure in words
+        :rtype:     int
+        """
+
+        # Build the k-gram rooted at start_idx
+        start_k_gram = " ".join(small_sequence[start_idx:start_idx+self.k])
+
+        # If the k-gram is in the big hash table,
+        if start_k_gram in self.index_hash:
+
+            longest = 0
+
+            # Scan over all indices in the list in the hash table
+            for word_seq_index in self.index_hash[start_k_gram]:
+
+                # While loop, starting at size k, expanding the length of the LCS until the sequences stop matching
+                i = self.k
+                while (word_seq_index + i < len(self.word_sequence) and # in-bounds on word_sequence
+                       start_idx + i < len(small_sequence) and # in-bounds on small_sequence
+                       self.word_sequence[word_seq_index + i] == small_sequence[start_idx + i]): # the words are the same
+                    i = i+1
+
+                # The length of the longest LCS seen so far is kept
+                longest = max(longest, i)
+            return longest
+        else:
+            # If the k-gram ain't in the big hash table, check if it's sub-grams are in the stored set
+            for i in range(self.k-1,0,-1): # In order from k-1 to 1
+                if " ".join(small_sequence[start_idx:start_idx+i]) in self.short_gram_set:
+                    return i
+            return 0
+
+    def lcs_length(self, small_sequence):
+        """
+        Given `small_sequence`, a list of strings, this returns the length of
+        the longest common substring between `small_sequence` and
+        `word_sequence`
+
+        :param      small_sequence:  A small sequence of words
+        :type       small_sequence:  List of strings
+
+        :returns:   Length of the LCS, measured in words
+        :rtype:     int
+        """
+        longest = 0
+
+        # Just iterative over all starting words in `small_sequence` and keep the biggest number
+        for i in range(len(small_sequence)):
+            sequence_len = self.length_of_lcs_from_start(small_sequence, i)
+            longest = max(longest, sequence_len)
+
+        return longest
+
+    # Given `small_sequence`, a list of strings, this prints highlighted markdown that shows where one substring of `word_sequence` ends and the next one starts. If a word doesn't appear in `word_sequence`, we just treat it like a substring of length 1.
+
+    def greedy_lcs_markdown(self, small_sequence):
+        """
+        Given `small_sequence`, a list of strings, this prints highlighted
+        markdown that shows where one substring of `word_sequence` ends and the
+        next one starts. If a word doesn't appear in `word_sequence`, we just
+        treat it like a substring of length 1.
+
+        :param      small_sequence:  A small sequence of words
+        :type       small_sequence:  List of strings
+
+        :returns:   Markdown display text (renders by default on Jupyter)
+        :rtype:     Markdown display text
+        """
+        list_of_sequences = []  # The output list
+
+        i = 0
+        while i < len(small_sequence):
+            # iteratate over small_sequence, finding the lcs rooted at index i,
+            # and color that LCS accordingly.
+            sequence_len = max(self.length_of_lcs_from_start(small_sequence, i), 1)
+            list_of_sequences.append(small_sequence[i:i+sequence_len])
+            i = i + sequence_len
+
+        return list_of_seqs_to_markdown(list_of_sequences)
